27cc9db4aa68af1612c615199a3fa2aa366706ef
NothNada <grubens476@gmail.com>
2025-03-20 14:33:26 -0300

    adicionando mutacoes

--- teste.cs
+++ teste.cs
@@ -32,12 +32,14 @@ class Program{
     private static readonly int NUMBER_OF_INPUT_NEURONS = 2;
     private static readonly int NUMBER_OF_OUTPUT_NEURONS = 4;
 
-    private static readonly int POPULACAO = 10;
+    private static readonly int POPULACAO = 10000;
     private static double[][] DnaDaVez = new double[POPULACAO][];
     private static IA[] Individuos = new IA[POPULACAO];
     private static IA MelhorIndividuo;
     private static int MelhorIndividuoI;
     private static int mortos = 0;
+    private static double RangeRandom;
+    private static Random rd = new Random();
 
     private static bool fimGer = false;
 
@@ -68,6 +70,11 @@ class Program{
             if(tecla.KeyChar == 'c'){
                 Console.Clear();
             }
+            if(tecla.KeyChar == 'q'){
+                fimGer = true;
+            }
+
+            VerificaFimDePartida();
 
         }
 
@@ -92,6 +99,88 @@ class Program{
         // {
         //     Console.WriteLine(valor);
         // }
+    }
+
+    static void RandomMutations(){
+        RangeRandom = Individuos[0].TamanhoDNA;
+
+        IA[] Vetor = new IA[POPULACAO];
+        IA Temp;
+
+        for(int i=0; i<POPULACAO; i++){
+            Vetor[i] = Individuos[i];
+        }
+
+        for(int i=0; i<POPULACAO; i++){
+            for(int j=0; j<POPULACAO-1; j++){
+                if(Vetor[j].Fitness < Vetor[j+1].Fitness){
+                    Temp = Vetor[j];
+                    Vetor[j] = Vetor[j+1];
+                    Vetor[j+1] = Temp;
+                }
+            }
+        }
+
+        int Step = 5;
+        for(int i=0; i<Step; i++){  /// Clonando individuos
+            for(int j=Step+i; j<POPULACAO; j=j+Step){
+                for(int k=0; k<Vetor[j].TamanhoDNA; k++){
+                    Vetor[j].DNA[k] = Vetor[i].DNA[k];        /// individuo 'j' recebe dna do individuo 'i'
+                }
+            }
+        }
+
+        for(int j=Step; j<POPULACAO; j++){
+            int tipo;
+            int mutations = rd.Next(1, (int)RangeRandom)+1;
+
+            for(int k=0; k<mutations; k++){
+                tipo = rd.Next(3);
+
+                int indice = rd.Next(Vetor[j].TamanhoDNA);
+                switch(tipo)
+                {
+                    case 0:
+                    {
+                        Vetor[j].DNA[indice] = Aleatorio.GetRandomValue();       /// Valor Aleatorio
+
+                    }   break;
+                    case 1:
+                    {
+                        double number = (rd.Next(10001) / 10000.0) + 0.5;
+                        Vetor[j].DNA[indice] = Vetor[j].DNA[indice]*number;   /// Multiplica��o aleatoria
+
+                    }   break;
+                    case 2:
+                    {
+                        double number = Aleatorio.GetRandomValue()/100.0;
+                        Vetor[j].DNA[indice] = Vetor[j].DNA[indice] + number; /// Soma aleatoria
+
+                    }   break;
+                }
+            }
+        }
+
+        for(int j=0; j<POPULACAO; j++){  /// Copiando novos DNAs para DNAsDaVez
+        
+            for(int k=0; k<Individuos[j].TamanhoDNA; k++){
+                DnaDaVez[j][k] = Individuos[j].DNA[k];
+            }
+        }
+
+        Console.WriteLine("Range Random: "+ RangeRandom);
+        RangeRandom = RangeRandom*0.99;
+        if(RangeRandom < 5){
+            RangeRandom = 5;
+        }
+
+    }
+
+    static void VerificaFimDePartida(){
+        if(mortos == POPULACAO || fimGer == true){
+            RandomMutations();
+            IniciarNovaPartida();
+        }
     }
 
     static void IniciarNovaPartida(){
@@ -159,6 +248,19 @@ class Program{
                 }
                 if(saida[3] > 0){
                     Individuos[i].y -= SPEED;
+                }
+
+                if(Individuos[i].x > LARGURA_JANELA){
+                    Individuos[i].live = false;
+                }
+                if(Individuos[i].x < 0){
+                    Individuos[i].live = false;
+                }
+                if(Individuos[i].y > ALTURA_JANELA){
+                    Individuos[i].live = false;
+                }
+                if(Individuos[i].y < 0){
+                    Individuos[i].live = false;
                 }
             }
         }
